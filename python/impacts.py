7c81b0e2821f2669c7807d9cdb1c97a77e3fcb82
Alex Kenan <alexkenan@users.noreply.github.com>
2021-03-09 17:18:01 -0500

    Create impacts.py

---
+++ python/impacts.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#####################################
+#    LAST UPDATED     09 MAR 2021   #
+#####################################
+"""
+Use Plotly for aircraft data analysis. Data from
+https://catalog.data.gov/dataset/p-3-meteorological-and-navigation-data-impacts-v1
+"""
+import plotly.graph_objects as go
+import pandas as pd
+
+list_of_files = [
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200112_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200118_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200125_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200201_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200205_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200207_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200213_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200218_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200220_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200224_R0.csv',
+     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200225_R0.csv']
+for i in range(len(list_of_files)):
+    df = pd.read_csv(list_of_files[i])
+    fig = go.Figure()
+    counter = 30
+    fig.add_trace(go.Scattermapbox(mode="lines", lon=df['Longitude'], lat=df['Latitude'], showlegend=False,
+                                   line={'color': 'gray'},
+                                   name=""))
+    fig.add_trace(go.Scattermapbox(mode="markers+lines", lon=df['Longitude'].head(counter),
+                                   lat=df['Latitude'].head(counter),
+                                   showlegend=True,
+                                   marker={'size': 6, 'color': 'blue'},
+                                   name="Start"))
+    fig.add_trace(go.Scattermapbox(mode="markers+lines", lon=df['Longitude'].tail(counter),
+                                   lat=df['Latitude'].tail(counter),
+                                   showlegend=True,
+                                   marker={'size': 6, 'color': 'red'},
+                                   name="End"))
+    fig.update_layout(
+        margin={'l': 0, 't': 0, 'b': 0, 'r': 0},
+        mapbox={'center': {'lon': -100, 'lat': 40},
+                'style': "carto-positron",
+                'zoom': 3},
+        geo_scope="usa")
+    fig.show()
