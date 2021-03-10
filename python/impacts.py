81179b2cda6394fa4a450821f4dcde978d79fb52
Alex Kenan <alexkenan@users.noreply.github.com>
2021-03-09 20:53:07 -0500

    Update impacts.py

--- python/impacts.py
+++ python/impacts.py
@@ -5,22 +5,35 @@
 """
 Use Plotly for aircraft data analysis. Data from
 https://catalog.data.gov/dataset/p-3-meteorological-and-navigation-data-impacts-v1
+https://catalog.data.gov/dataset/er-2-navigation-data-impacts-v1
+
 """
 import plotly.graph_objects as go
 import pandas as pd
 
 list_of_files = [
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200115_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200118_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200125_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200201_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200205_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200207_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200223_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200225_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200227_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_ER2_20200302_R0.csv',
     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200112_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200118_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200125_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200201_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200205_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200207_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200213_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200218_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200220_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200224_R0.csv',
-     'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200225_R0.csv']
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200118_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200125_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200201_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200205_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200207_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200213_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200218_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200220_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200224_R0.csv',
+    'https://raw.githubusercontent.com/alexkenan/nasa_impacts/main/datasets/IMPACTS_MetNav_P3B_20200225_R0.csv']
+
 for i in range(len(list_of_files)):
     df = pd.read_csv(list_of_files[i])
     fig = go.Figure()
